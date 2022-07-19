f8333194fc4fbee72bc691612021d992e9ed8bab
George Ngozi Favour <georgengozi019@gmail.com>
2022-07-19 13:28:20 +0100

    updated securities

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './containers/App';
-import registerServiceWorker from './registerServiceWorker';
-import 'tachyons'; 
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./containers/App";
+import registerServiceWorker from "./registerServiceWorker";
+import "tachyons";
 
+// ReactDOM.render(<App />, document.getElementById("root"));
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
 // registerServiceWorker();
